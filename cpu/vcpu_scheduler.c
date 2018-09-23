1a05594b362bc5c72e3560244a64e20834f09e90
Sonia Mathew <sonisvm@gmail.com>
2018-09-23 12:34:34 -0700

    added log files for cpu

--- cpu/vcpu_scheduler.c
+++ cpu/vcpu_scheduler.c
@@ -17,7 +17,6 @@ typedef struct domainStats {
 typedef domainStats * domainStatsPtr;
 
 typedef struct pcpuStats {
-	unsigned long long time;
 	double usage;
 } pcpuStats;
 
@@ -32,25 +31,17 @@ static int calculateDomainUsage(domainStatsPtr currDomainStats, domainStatsPtr p
 	return 0;
 }
 
-static int calculatePcpuUsage(pcpuStatsPtr currPcpuStats, pcpuStatsPtr prevPcpuStats, int numPcpus, int t){
 
-	for(int i=0; i<numPcpus; i++){ //0 corresponds to cpu0, 1 to cpu1 and so on
-		currPcpuStats[i].usage = 100*(currPcpuStats[i].time - prevPcpuStats[i].time)/(t*pow(10, 9));
-		printf("cpu=%d, curr=%lld, prev=%lld\n", i, currPcpuStats[i].time, prevPcpuStats[i].time);
-	}
-	
-	return 0;
-}
-
-static void getPcpuUsage(domainStatsPtr currDomainStats,pcpuStatsPtr currPcpuStats, int numDomains){
+static void calculatePcpuUsage(domainStatsPtr currDomainStats,pcpuStatsPtr currPcpuStats, int numDomains ){
 	for(int i=0; i<numDomains; i++){
 		virVcpuInfoPtr info = malloc(sizeof(virVcpuInfo));
 		virDomainGetVcpus(currDomainStats[i].domain, info, 1, NULL, 0);
-		
-		currPcpuStats[info->cpu].time += currDomainStats[i].time; 
-		printf("time for pcpu%d is %lld\n", info->cpu, currPcpuStats[info->cpu].time);
+		currPcpuStats[info->cpu].usage += currDomainStats[i].usage; 
+		printf("usage for pcpu%d is %lf\n", info->cpu, currPcpuStats[info->cpu].usage);
 	}
 }
+
+
 
 static domainStats getCurrVcpuTime(virDomainPtr domain){
 	domainStats d;
@@ -62,7 +53,7 @@ static domainStats getCurrVcpuTime(virDomainPtr domain){
 
 	//get the cpu_time
 	//Assumption is that there is only one vcpu
-	unsigned long long cpuTime, userTime, systemTime;
+	unsigned long long cpuTime;
 	for(int i=0; i<nparams; i++){
 		if(strcmp(params[i].field, "cpu_time")==0){
 			cpuTime = params[i].value.ul;
@@ -70,42 +61,45 @@ static domainStats getCurrVcpuTime(virDomainPtr domain){
 		}
 
 	}
-	/*system_time + user_time is giving the overhead of QEMU / KVM on the host side. And cpu_time - (user_time + guest_time) is giving the actual amount of time the guest OS was running its CPUs.*/
+
 	d.time = cpuTime ;
 	d.usage = 0.0;
 	return d;
 }
 
 static void pinVcpuToPcpu(domainStatsPtr currDomainStats, pcpuStatsPtr currPcpuStats, int numDomains, int numPcpus){
-	double usage = currPcpuStats[0].usage;
-	printf("cpu=%d, usage=%f\n", 0, currPcpuStats[0].usage);
-	int freestPcpu = 0;int stop=1;
+	double maxUsage = currPcpuStats[0].usage, minUsage = currPcpuStats[0].usage;
+
+	int freestPcpu = 0, busiestPcpu=0;
+
 	for(int i=1; i<numPcpus; i++){
-		printf("cpu=%d, usage=%f\n", i, currPcpuStats[i].usage);
-		//checking if there is any pcpu which is a lot freer than rest
-		double diff = currPcpuStats[i].usage > usage? currPcpuStats[i].usage - usage: usage - currPcpuStats[i].usage;
-		if(diff > 10.0){
-			stop =0;
-		}
-			
-		if(currPcpuStats[i].usage < usage){
+	
+		if(currPcpuStats[i].usage < minUsage){
 			freestPcpu = i;
-			usage = currPcpuStats[i].usage;
+			minUsage = currPcpuStats[i].usage;
+		}
+		if(currPcpuStats[i].usage > maxUsage){
+			busiestPcpu = i;
+			maxUsage = currPcpuStats[i].usage;
 		}
 	}
-
-	if(stop==1) {
+	printf("maxUsage=%lf, minUsage=%lf\n", maxUsage, minUsage);
+	if(maxUsage - minUsage <= 35.0) {
 		printf("The pcpus are balanced\n");
 		return;
 	}	
 
-	//find the domain with heaviest load
-	int busiestDomain = -1;
-	usage = INT_MIN;
+	//find the domain with heaviest and freest load
+	int busiestDomain = -1, freestDomain=-1;
+	maxUsage = INT_MIN, minUsage = INT_MAX;
 	for(int i=0; i<numDomains; i++){
-		if(currDomainStats[i].usage > usage){
+		if(currDomainStats[i].usage > maxUsage){
 			busiestDomain = i;
-			usage = currDomainStats[i].usage;
+			maxUsage = currDomainStats[i].usage;
+		}
+		if(currDomainStats[i].usage < minUsage){
+			freestDomain = i;
+			minUsage = currDomainStats[i].usage;
 		}
 	}
 	if(busiestDomain ==-1) return;
@@ -118,9 +112,12 @@ static void pinVcpuToPcpu(domainStatsPtr currDomainStats, pcpuStatsPtr currPcpuS
 	if(virDomainPinVcpu(currDomainStats[busiestDomain].domain, 0, &cpuMap, (numPcpus/8)+1)==-1){
 		printf("Unable to pin vcpu to pcpu");
 	}
-	
-	virDomainGetVcpuPinInfo(currDomainStats[busiestDomain].domain, 1, &cpuMap, (numPcpus/8)+1, 0);
-	printf("After changing%d\n", cpuMap);
+
+	cpuMap = 0x1 << busiestPcpu;
+	printf("cpuMap=%d\n", cpuMap);fflush(stdout);
+	if(virDomainPinVcpu(currDomainStats[freestDomain].domain, 0, &cpuMap, (numPcpus/8)+1)==-1){
+		printf("Unable to pin vcpu to pcpu");
+	}
 }
 
 
@@ -138,7 +135,7 @@ int main(int argc, char* argv[]){
 	domainStatsPtr prevDomainStats = NULL;
 	pcpuStatsPtr prevPcpuStats = NULL;
 	int prevNumDomains;
-	int prevNumPcpu ;
+	//int prevNumPcpu ;
 
 	//get stats for all the domains
 	virDomainPtr * activeDomains = NULL;
@@ -151,6 +148,7 @@ int main(int argc, char* argv[]){
 	while(1){
 
 		domainStatsPtr currDomainStats = malloc(numDomains*sizeof(domainStats));
+		memset(currDomainStats, 0, numDomains*sizeof(domainStats));
 		for(int i=0; i<numDomains; i++){
 			currDomainStats[i] = getCurrVcpuTime(activeDomains[i]);
 			printf("domain=%d, cputime =%lld\n", i, currDomainStats[i].time);fflush(stdout);
@@ -165,22 +163,16 @@ int main(int argc, char* argv[]){
 			numPcpus = nodeInfo->cpus;
 			currPcpuStats = malloc(numPcpus*sizeof(pcpuStats));
 			memset(currPcpuStats, 0, numPcpus*sizeof(pcpuStats));
-			getPcpuUsage(currDomainStats, currPcpuStats, numDomains);
+
 		}
-		if(prevDomainStats==NULL){
-			printf("prevDomainStats is null\n");fflush(stdout);
-		}
-		if(prevPcpuStats==NULL){
-			printf("prevPcpuStats is null\n");fflush(stdout);
-		}
-		if(prevDomainStats!=NULL && prevPcpuStats!=NULL){
+
+		if(prevDomainStats!=NULL){
 			calculateDomainUsage(currDomainStats, prevDomainStats, timeInterval, numDomains);
-			calculatePcpuUsage(currPcpuStats, prevPcpuStats, numPcpus, timeInterval);
+
+			calculatePcpuUsage(currDomainStats, currPcpuStats, numDomains);
 			pinVcpuToPcpu(currDomainStats, currPcpuStats, numDomains, numPcpus);
 		}
-		prevNumPcpu = numPcpus;
-		prevPcpuStats = malloc(numPcpus*sizeof(pcpuStats));
-		memcpy(prevPcpuStats, currPcpuStats, numPcpus * sizeof(pcpuStats));
+
 		free(currPcpuStats);
 
 		prevNumDomains = numDomains;
